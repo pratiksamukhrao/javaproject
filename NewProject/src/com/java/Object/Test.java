1ec2cd07293240ea8135271ff420ab86fd351b08
Lenovo <Lenovo@DESKTOP-5H4VDU0>
2022-09-20 16:03:25 +0530

    javacode

---
+++ NewProject/src/com/java/Object/Test.java
@@ -0,0 +1,12 @@
+package com.java.Object;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hii java code");
+		// TODO Auto-generated method stub
+
+	}
+
+}
